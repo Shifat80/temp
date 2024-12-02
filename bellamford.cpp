31bfaed6484089a48d2a181b0ebd9c549261c8cd
Mehedi Hasan shifat <107384871+Shifat80@users.noreply.github.com>
2024-12-03 01:33:12 +0600

    bellamford.cpp

--- bellamford.cpp
+++ bellamford.cpp
@@ -1,10 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bellmanFord(int V, int E, vector<vector<int>>& edges, int start) {
+int main() {
+    int V, E;
+    cin >> V >> E;
+    vector<vector<int>> edges;
+
+    for (int i = 0; i < E; i++) {
+        int u, v, weight;
+        cin >> u >> v >> weight;
+        edges.push_back({u, v, weight});
+    }
+
+    int start;
+    cin >> start; 
+
+   
     vector<int> dist(V, INT_MAX);
     dist[start] = 0;
 
+    // Relax all edges |V| - 1 times
     for (int i = 1; i <= V - 1; i++) {
         for (int j = 0; j < E; j++) {
             int u = edges[j][0];
@@ -17,7 +32,6 @@ void bellmanFord(int V, int E, vector<vector<int>>& edges, int start) {
         }
     }
 
-    // Check for negative-weight cycles
     for (int j = 0; j < E; j++) {
         int u = edges[j][0];
         int v = edges[j][1];
@@ -25,28 +39,17 @@ void bellmanFord(int V, int E, vector<vector<int>>& edges, int start) {
 
         if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) {
             cout << "Graph contains negative weight cycle" << endl;
-            return;
+            return 0;
         }
     }
 
-    // Print distances
     for (int i = 0; i < V; i++) {
-        cout << "Vertex " << i << " Distance: " << dist[i] << endl;
+        if (dist[i] == INT_MAX) {
+            cout << "Vertex " << i << ": INF" << endl;
+        } else {
+            cout << "Vertex " << i << ": " << dist[i] << endl;
+        }
     }
-}
-
-int main() {
-    int V, E;
-    cin >> V >> E;
-    vector<vector<int>> edges;
-
-    for (int i = 0; i < E; i++) {
-        int u, v, weight;
-        cin >> u >> v >> weight;
-        edges.push_back({u, v, weight});
-    }
-
-    bellmanFord(V, E, edges, 0);
 
     return 0;
 }
